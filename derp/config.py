55116e6a7aecfc0adb373c5a7a56ea9ac61c9eab
chriscauley <chris@lablackey.com>
2018-01-21 23:28:50 +0000

    moving .derp to _derp, setting STABLE off COMMIT_HASH and putting COMMIT_HASH on config

--- derp/config.py
+++ derp/config.py
@@ -1,12 +1,12 @@
-import os, sys
+import os, subprocess, sys
 
 DJANGO_SETTINGS_MODULE = "settings"
 URLS = []
 URL_GROUPS = []
 EMAILS = []
 PREP_VARIABLES = lambda *args,**kwargs: None
-STABLE="--stable" in sys.argv
-derp_dir = os.path.join(os.path.abspath("."),'.derp')
-
+derp_dir = os.path.join(os.path.abspath("."),'_derp')
+COMMIT_HASH = subprocess.Popen(['git','rev-parse','HEAD'],stdout=subprocess.PIPE).communicate()[0].strip()
 f = os.path.join(derp_dir,"config.py")
 exec(compile(open(f).read(),f, 'exec'), globals(), locals())
+STABLE = COMMIT_HASH == STABLE_HASH
