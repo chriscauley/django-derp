df429619e5478b58b3fee1c26e5ee8e54acb8ec3
chriscauley <chris@lablackey.com>
2018-01-21 21:30:26 +0000

    changing how stable loads

--- derp/config.py
+++ derp/config.py
@@ -1,10 +1,11 @@
-import os, argparse
+import os, sys
 
 DJANGO_SETTINGS_MODULE = "settings"
 URLS = []
+URL_GROUPS = []
 EMAILS = []
 PREP_VARIABLES = lambda *args,**kwargs: None
-STABLE=os.environ.get("STABLE",False)
+STABLE="--stable" in sys.argv
 derp_dir = os.path.join(os.path.abspath("."),'.derp')
 
 f = os.path.join(derp_dir,"config.py")
