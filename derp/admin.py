fab6e3a89cdeb75302507140287b8c6acae6eb48
chriscauley <chris@lablackey.com>
2018-01-26 01:13:35 +0000

    adding registering new models

--- derp/admin.py
+++ derp/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 
-from models import Test, TestRun
+from models import Commit, Test, TestStatus, TestRun
+
+@admin.register(Commit)
+class CommitAdmin(admin.ModelAdmin):
+    pass
+
+class TestRunInline(admin.TabularInline):
+    extra = 0
+    model = TestRun
+    readonly_fields = ['commit_id','queries','milliseconds']
 
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
+    inlines = [TestRunInline]
+    readonly_fields = ['type','name','parameters','parameters_hash','result']
+
+@admin.register(TestStatus)
+class TestStatusAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(TestRun)
 class TestRunAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ['commit_id']
+    list_display = ['__unicode__','commit_id','milliseconds','queries']
