6a2f8524a1eb828e999a0452d4b446c76ce42c7a
chriscauley <chris@lablackey.com>
2018-01-26 01:14:31 +0000

    using runners in run_derp

--- derp/management/commands/run_derp.py
+++ derp/management/commands/run_derp.py
@@ -3,32 +3,34 @@ from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 from derp import config, t
+from derp.runner import URLRunner
 
 class Command (BaseCommand):
     def add_arguments(self,parser):
-        parser.add_argument('command_name',nargs="*",type=str)
-        parser.add_argument('--email',help="which email group to use")
-        parser.add_argument('--url',help="which email group to use")
-        parser.add_argument( #not, this is used as sys.argv in config.py
-            '--stable',
-            action='store_true',
-            dest='stable',
-            help='Accept current code as stable, overwriting all the currently stored results.',
-        )
+        parser.add_argument('command_name',nargs='?',type=str)
+        parser.add_argument('target',nargs='?',type=str,help="which email group to use")
+    def print_status(self):
+        command_tuples = sorted([(key,value) for key,value in config.COMMANDS.items() if not key.startswith("_")])
+        for key,commands in command_tuples:
+            print key,'\n'
+            print "{0:10.10} ".format(key),len(commands)
+        exit()
     def handle(self, *args, **kwargs):
-        commands = kwargs['command_name'] or sorted(config.URL_GROUPS.keys())
-        emails = config.EMAILS
-        if kwargs['email'] in config.EMAIL_GROUPS:
-            emails = config.EMAIL_GROUPS[kwargs['email']]
-        elif kwargs['email']:
-            emails = [e for e in emails if kwargs['email'] in e]
-        print "Using urls:",commands
-        print "Using %s emails"%len(emails)
-        for email in emails:
-            t.login(email)
-            t.clear(email)
-            for command in commands:
-                urls = config.URL_GROUPS[command]
-                for url in urls:
-                    d = config.PREP_VARIABLES(email=email,url=url)
-                    t.verify_url(url,d)
+        from _derp.runner import TaskRunner
+        command = kwargs['command_name'] or '_all'
+        if command == 'status':
+            self.print_status()
+        targets = config.ALLOWED_TARGETS.get(command,None) # this means a command is limited to these arguments
+        targets = targets or config.TARGETS.get(kwargs['target'],kwargs['target']) # or the specified arguments
+        targets = targets or config.TARGETS['_all'] # or just use them all!
+        if type(targets) == str:
+            targets = targets.split(',')
+        print "Using command:",command
+        print "Using targets:",targets
+        for target in targets:
+            t.clear("\n*** Using target %s ***"%target)
+            for subcommand in config.COMMANDS.get(command,[]):
+                if type(subcommand) == str:
+                    URLRunner(subcommand,email=target).run()
+                else:
+                    TaskRunner(subcommand,target).run()
