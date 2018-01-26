a1b7b38ea6c5f38c73194cc71f5c770006e1b19c
chriscauley <chris@lablackey.com>
2018-01-26 01:14:07 +0000

    adding a few more config variables, renaming others

--- derp/config.py
+++ derp/config.py
@@ -1,12 +1,17 @@
-import os, subprocess, sys
+import os, subprocess, sys, hashlib
 
 DJANGO_SETTINGS_MODULE = "settings"
-URLS = []
-URL_GROUPS = []
-EMAILS = []
+TARGETS = {}
+COMMANDS = {}
+ALLOWED_TARGETS = {}
 PREP_VARIABLES = lambda *args,**kwargs: None
 derp_dir = os.path.join(os.path.abspath("."),'_derp')
+
+_cmd = 'git log --pretty=format:%H'
+COMMIT_LIST = subprocess.Popen(_cmd.split(" "),stdout=subprocess.PIPE).communicate()[0].split("\n")
 COMMIT_HASH = subprocess.Popen(['git','rev-parse','HEAD'],stdout=subprocess.PIPE).communicate()[0].strip()
+diff_string,_error = subprocess.Popen(['git','diff'],stdout=subprocess.PIPE).communicate()
+UNSTAGED_HASH = diff_string and hashlib.md5(diff_string).hexdigest() # empty string or a hash of the unstaged code
 f = os.path.join(derp_dir,"config.py")
 exec(compile(open(f).read(),f, 'exec'), globals(), locals())
-STABLE = COMMIT_HASH == STABLE_HASH
+STABLE = UNSTAGED_HASH + COMMIT_HASH == STABLE_HASH
