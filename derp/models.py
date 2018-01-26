fd1789e4340a70c4ae595b2f27c80b61c6607f51
chriscauley <chris@lablackey.com>
2018-01-26 01:04:22 +0000

    adding commit model and TestStatus

--- derp/models.py
+++ derp/models.py
@@ -3,7 +3,42 @@ from django.db import models
 from derp.fields import JSONField
 from derp import config
 
-import hashlib, json
+import hashlib, json, subprocess
+
+class JsonModel(models.Model):
+    class Meta:
+        abstract=True
+    json_fields = []
+    @property
+    def as_json(self):
+        return {k:getattr(self,k) for k in self.json_fields}
+
+class CommitManager(models.Manager):
+    def refresh(self):
+        self.all().update(parent=None)
+        _cmd = 'git log --pretty=format:%H'
+        hashes = subprocess.Popen(_cmd.split(" "),stdout=subprocess.PIPE).communicate()[0].split("\n")
+        parent = None
+        for _hash in hashes:
+            commit, new = self.get_or_create(id=_hash,defaults={'order':0})
+            commit.parent = parent
+            commit.order = parent.order + 1
+            commit.save()
+            parent = commit
+
+class Commit(JsonModel):
+    class Meta:
+        ordering = ("-order",)
+    __unicode__ = lambda self: self.id
+    json_fields = ['parent_id','order','id']
+    parent = models.ForeignKey('self', related_name='children', blank=True, null=True)
+    order = models.IntegerField(default=0)
+    id = models.CharField(max_length=32,primary_key=True)
+    objects = CommitManager()
+    def save(self,*args,**kwargs):
+        if self.parent_id:
+            self.order = self.parent.order + 1
+        super(Commit,self).save(*args,**kwargs)
 
 class TestManager(models.Manager):
     def get_from_parameters(self,*args,**kwargs):
@@ -14,12 +49,12 @@ class TestManager(models.Manager):
         obj.save()
         return obj,new
 
-class Test(models.Model):
-    class Meta:
-        app_label = "derp"
+class Test(JsonModel):
+    json_fields = ['id','type','name','parameters','result']
     objects = TestManager()
     TEST_CHOICES = [
-        ('url','url'),
+        ('url','URL'),
+        ('task','TASK'),
     ]
     type = models.CharField(max_length=16,choices=TEST_CHOICES)
     name = models.CharField(max_length=128,help_text="Verbose Test being called (eg. a url, task, or function")
@@ -28,22 +63,30 @@ class Test(models.Model):
     result = models.TextField(default="")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    @property
+    def as_json(self):
+        return { k: getattr(self,k) for k in ['type','name','parameters','updated',"id"]}
     def save(self,*args,**kwargs):
         self.parameters_hash = hashlib.md5(json.dumps(self.parameters,sort_keys=True)).hexdigest()
         super(Test,self).save(*args,**kwargs)
     def verify(self,content):
         from derp import t
+        old_result = self.result or ""
+        success = False
         if config.STABLE:
+            verb = "SAME" if self.result == content else "UPDATED"
+            if not old_result:
+                verb = "WROTE"
             self.result = content
             self.save()
-            t("wrote %s"%self)
-            return True
+            t("%s %s"%(verb,self))
+            success = True
         elif not self.result:
             raise ValueError("Stable result not recorded for %s"%self)
         elif self.result == content:
             t("PASS: %s"%self)
-            return True
-        else:
+            success = True
+        if old_result and old_result != content:
             # maybe this should be on t?
             import os, difflib
             name = str(self).replace(" ","/").replace("#","")
@@ -51,15 +94,17 @@ class Test(models.Model):
             fname = name.split("/")[-1]
             diff_dir = t.mkdir(diff_dir,prefix=".diff")
             diff_path = os.path.join(diff_dir,"%s.diff"%fname)
-            d = "\n".join(difflib.unified_diff(self.result.split("\n"),content.split("\n")))
+            d = "\n".join(difflib.unified_diff(old_result.split("\n"),content.split("\n")))
             t.write_file(diff_path,d,group="DIFF")
             t("CHANGED: %s %s"%(self.id,self))
-    def record_run(self,seconds,queries,git_hash):
+        return success
+    def record_run(self,seconds,queries,status='success'):
         run = TestRun.objects.create(
             test=self,
-            commit_hash=git_hash,
+            commit_id=config.COMMIT_HASH,
             queries=queries,
-            milliseconds=int(1000*seconds)
+            milliseconds=int(1000*seconds),
+            status=status
         )
     def get_short_url(self):
         url = self.name.split("?")[0]
@@ -75,16 +120,43 @@ class Test(models.Model):
         username,domain = self.parameters['email'].split("@")
         return "%s@%s"%(username[0],domain)
     def __unicode__(self):
-        if self.name.startswith("/") and 'email' in self.parameters: #probably a url
+        if self.name.startswith("/") and '@' in self.parameters.get("email",""): #probably a url
             return "%s: %s %s"%(self.type,self.get_short_url(),self.get_short_email())
         return self.name
 
+STATUS_CHOICES = [
+    ('pass','pass'),
+    ('fail','fail'),
+    ('unknown','unknown'),
+]
 
-class TestRun(models.Model):
-    class Meta:
-        app_label = "derp"
+class TestStatus(JsonModel):
+    __unicode__ = lambda self: "Test #%s @%s: %s"%(self.test_id,self.commit,self.status) 
+    json_fields = ['id','test_id','commit_id','status','average_ms','run_count']
     test = models.ForeignKey(Test)
-    commit_hash = models.CharField(max_length=32)
+    commit = models.ForeignKey('derp.Commit')
+    status = models.CharField(max_length=16,choices=STATUS_CHOICES,default='unknown')
+    average_ms = models.IntegerField(default=0)
+    run_count = models.IntegerField(default=0)
+    def save(self,*args,**kwargs):
+        times = TestRun.objects.filter(test_id=self.test_id,commit_id=self.commit_id,status='pass')
+        times = list(times.values_list('milliseconds',flat=True))
+        self.average_ms = sum(times)/len(times) if times else 0
+        self.run_count = len(times)
+        super(TestStatus,self).save()
+
+class TestRun(JsonModel):
+    __unicode__ = lambda self: "%s"%self.test_id
+    test = models.ForeignKey(Test)
+    commit = models.ForeignKey('derp.Commit',null=True,blank=True)
     queries = models.IntegerField()
     milliseconds = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=16,choices=STATUS_CHOICES,default='unknown')
+    def save(self,*args,**kwargs):
+        if not self.id:
+            status,new = TestStatus.objects.get_or_create(test=self.test,commit=self.commit)
+            if teststatus.status != self.status:
+                teststatus.status = self.status
+                teststatus.save()
+        super(TestRun,self).save(*args,**kwargs)
